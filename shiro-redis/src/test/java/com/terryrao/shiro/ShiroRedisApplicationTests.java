7d0bca4c5494acca7e4396ca9f8941339c21022e
raowei <raowei@88bank.com>
2018-06-07 19:45:22 +0800

    init project

---
+++ shiro-redis/src/test/java/com/terryrao/shiro/ShiroRedisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.terryrao.shiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShiroRedisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
